68ec9455ae8f1d8cceae312bab322392596110f7
Gritsus K <gritsus@gmail.com>
2015-06-23 17:00:50 +0600

    i'm dummy

---
+++ src/study/MergeSort.java
@@ -0,0 +1,63 @@
+package study;
+
+public class MergeSort extends Sort{
+
+	static Comparable[] buffer;
+	
+	public static void main(String[] args){
+		Comparable[] array = Randomizer.getArray(100);
+		sort(array);
+		print(array);
+		System.out.println(isSort(array));
+	}
+	
+	
+	/**
+	 * Слияние части массива
+	 * !порядок индексов не проверяется
+	 * (возможен цикл с nullPointer при неправельных low mid high)
+	 * 
+	 * @param array начальный массив
+	 * @param low нижний индекс
+	 * @param mid средний индекс
+	 * @param high верхний индекс
+	 */
+	public static void merge(Comparable[] array, int low, int mid, int high){
+		if (low > mid || mid > high) return;
+		int firstIndex = mid;
+		int secondIndex = mid+1;
+		for (int k = 0; k < high - low; k++){
+			buffer[k] = array[k];
+		}
+		for (int k = low; k < high; k++){
+			if (firstIndex > mid)			array[k] = buffer[secondIndex++];
+			else if (secondIndex > high)	array[k] = buffer[firstIndex++];
+			else if (less(array, firstIndex, secondIndex))	array[k] = buffer[firstIndex++];
+			else 							array[k] = buffer[secondIndex++];
+		}
+	}
+	/**
+	 * Сортировка массива
+	 * 
+	 * @param array
+	 */
+	public static void sort(Comparable[] array){
+		buffer = new Comparable[array.length];
+		sort(array, 0, array.length - 1);
+	}
+	/**
+	 * Частичная сортировка массива
+	 * 
+	 * @param array
+	 * @param low
+	 * @param high
+	 */
+	public static void sort(Comparable[] array, int low, int high){
+		if (high <= low) return;
+		int mid = low + (high - low) / 2;
+		sort(array, low, mid);
+		sort(array, mid + 1, high);
+		merge(array, low, mid, high);
+		
+	}
+}
--- src/study/ShellSort.java
+++ src/study/ShellSort.java
@@ -5,6 +5,7 @@ public class ShellSort extends Sort{
 	
 	/**
 	 * меняет множитель шага
+	 * =(
 	 * 
 	 * @param multiplier новый множитель шага
 	 */
@@ -23,12 +24,15 @@ public class ShellSort extends Sort{
 		int N = array.length;
 		int h = 1;
 		while (h < N/multiplier){h = multiplier*h + 1;}
+
 		while (h >= 1){
 			for (int i = h; i < N; i++){
-				for (int j = i; j>=h && less(array, j, j-h); j -=h ){
+				for (int j = i; j>=h && less(array, j, j-h); j-=h ){
 					exch(array, j, j-h);
 				}
 			}
+			h /= 3;
 		}
+		
 	}
 }
